ed28695b07b93ec2104a2cc55dc95c3373a70d17
Hyder Ali <haiderbrohi222@gmail.com>
2025-10-29 13:37:50 +0500

    NetworkUtils

---
+++ app/src/main/java/com/example/tezgamdelivery/presenatation/utils/NetworkUtils.kt
@@ -0,0 +1,19 @@
+package com.example.tezgamdelivery.presenatation.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+object NetworkUtils {
+
+    fun isInternetAvailable(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork ?: return false
+        val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+        return activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+    }
+}
